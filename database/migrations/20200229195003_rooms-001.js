abd5c53fc410930d95ca464d9bd217e58077802d
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-01 10:08:54 -0600

    built the rooms migrations

--- database/migrations/20200229195003_rooms-001.js
+++ database/migrations/20200229195003_rooms-001.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('rooms', tbl => {
+        tbl.increments();
+        tbl.integer('room_id').notNullable();
+        tbl.string('title').notNullable();
+        tbl.string('description');
+        tbl.string('coordinates');
+        tbl.integer('elevation');
+        tbl.string('terrain');
+        tbl.boolean('n_exit').defaultTo(false);
+        tbl.boolean('s_exit').defaultTo(false);
+        tbl.boolean('w_exit').defaultTo(false);
+        tbl.boolean('e_exit').defaultTo(false);
+        
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schemda.dropTableIfExits('rooms')
 };
